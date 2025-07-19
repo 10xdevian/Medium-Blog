796a4fa197a6519cdf1761a5f303fd26b5165789
macOS <vikram.kr.gupta01@gmail.com>
2025-07-20 03:19:08 +0530

    folder stru

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,13 +1,23 @@
 import { Context } from "hono";
+import prisma from "../db";
+import { sign } from "hono/jwt";
+import { JWT_SECRET } from "../../config";
 
-export const signup = async (c: Context) => {
+export const signin = async (c: Context) => {
   return c.json({ message: "User created successfully" });
 };
 
-export const signin = async (c: Context) => {
-  return c.json({
-    msg: "User Signin",
+export const signup = async (c: Context) => {
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
   });
+
+  const token = await sign({ id: user.id }, JWT_SECRET);
+  return c.json({ token });
 };
 
 export const me = async (c: Context) => {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
 import { Hono } from "hono";
-// import { PrismaClient } from "@prisma/client";
-// import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { DATABASE_URL, JWT_SECRET } from "../config";
+import { DATABASE_URL} from "../config";
+import blogRoutes from "./routes/blog.routes";
+import userRoutes from "./routes/user.routes";
 
 const prisma = new PrismaClient({
   datasourceUrl: DATABASE_URL,
@@ -12,20 +12,8 @@ const prisma = new PrismaClient({
 
 const app = new Hono();
 
-app.post("/signup", async (c) => {
-  const body = await c.req.json();
-
-  const user = await prisma.user.create({
-    data: {
-      email: body.email,
-      password: body.password,
-    },
-  });
-
-  const token = await sign({ id: user.id }, JWT_SECRET);
-
-  return c.json({ token });
-});
+app.route("api/v1/auth", userRoutes);
+app.route("api/v1/blog", blogRoutes);
 
 app.get("/api", (c) => {
   return c.json({
